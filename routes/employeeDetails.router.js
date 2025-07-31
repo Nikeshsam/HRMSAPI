009b4c52ebf47ab602d7d43b517ef41cc00d14ac
Srini255 <srinitamil17@gmail.com>
2025-08-01 00:42:50 +0530

    changed routes

--- routes/employeeDetails.router.js
+++ routes/employeeDetails.router.js
@@ -3,19 +3,22 @@ import authorize from '../middlewares/auth.middleware.js';
 import { createOrUpdateEmployeeBasicDetails, getEmployeeBasicDetails } from '../controllers/employeeBasic.controller.js';
 import { createOrUpdateEmployeeContactDetails, getEmployeeContactDetails } from '../controllers/employeeBasic.controller.js';
 import { createOrUpdateDependentDetails, getDependentDetails } from '../controllers/employeeDependent.controller.js';
+import { createOrUpdateEducationDetails, getEducationDetails } from '../controllers/employeeEducation.controller.js';
 const employeeDetailsRouter = Router();
 
 
 employeeDetailsRouter.post('/basic-details',authorize,createOrUpdateEmployeeBasicDetails);
 employeeDetailsRouter.post('/contact-details',authorize,createOrUpdateEmployeeContactDetails);
 employeeDetailsRouter.post('/dependents',authorize,createOrUpdateDependentDetails);
-// employeeDetailsRouter.post('/education-details',authorize,createOrUpdateEmployeeEducationDetails);
+employeeDetailsRouter.post('/education-details',authorize,createOrUpdateEducationDetails);
+
+
 
 
 employeeDetailsRouter.get('/basic-details',authorize,getEmployeeBasicDetails);
 employeeDetailsRouter.get('/contact-details',authorize,getEmployeeContactDetails);
 employeeDetailsRouter.get('/dependents',authorize,getDependentDetails);
-// employeeDetailsRouter.get('/education-details',authorize,getEmployeeEducationDetails);
+employeeDetailsRouter.get('/education-details',authorize,getEducationDetails);
 
 
 
